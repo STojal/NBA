d8f378902aeca22a1a2adf8c2377ef3cf764023a
STojal <simaotojal@ua.pt>
2023-12-12 00:05:29 +0000

    Dark mode completo e inicio de fav

--- Players.js
+++ Players.js
@@ -128,24 +128,40 @@ var vm = function () {
 $(document).ready(function () {
     console.log("ready!");
     ko.applyBindings(new vm());
-});
+
+    
+        });
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 $(document).ajaxComplete(function (event, xhr, options) {
     $("#myModal").modal('hide');
 })
 
 function add_player() {
-
-    var lista = JSON.parse(localStorage.getItem("Lista")) || [];
+    var jogadores = JSON.parse(localStorage.getItem("jogadores")) || [];
     var name = event.target.id
     var id = name.split("_")
-    console.log(Array.isArray(lista));
-    id = id[1]
-    if (!lista.includes(id)) {
 
-        lista.push(id);
-        console.log(lista)
-        lista = localStorage.setItem("Lista", JSON.stringify(lista))
+    console.log(Array.isArray(jogadores));
+    id = id[1]
+    if (!jogadores.includes(id)) {
+
+        jogadores.push(id);
+        console.log(jogadores)
+        jogadores = localStorage.setItem("jogadores", JSON.stringify(jogadores))
 
         alert("Jogador adicionado aos favoritos")
     }
@@ -154,143 +170,3 @@ function add_player() {
     }
 
 };
-
-$(document).ready(function () {
-    var lista = JSON.parse(localStorage.getItem("Lista")) || [];
-    if (lista.length != 0) {
-        $(".favourites").show()
-        for (i in lista) {
-            var vm = function () {
-                console.log('ViewModel initiated...');
-                //---Variáveis locais
-                var self = this;
-                self.baseUri = ko.observable('http://192.168.160.58/NBA/api/Players/');
-                self.displayName = 'NBA Player Details';
-                self.error = ko.observable('');
-                self.passingMessage = ko.observable('');
-                //--- Data Record
-                self.Id = ko.observable('');
-                self.Name = ko.observable('');
-                self.CountryId = ko.observable('');
-                self.CountryName = ko.observable('');
-                self.PositionId = ko.observable('');
-                self.PositionName = ko.observable('');
-                self.Photo = ko.observable('');
-
-                //--- Page Events
-                self.activate = function (id) {
-                    console.log('CALL: getPlayer...');
-                    var composedUri = self.baseUri() + id;
-                    ajaxHelper(composedUri, 'GET').done(function (data) {
-                        console.log(data);
-                        hideLoading();
-
-                        self.Id(data.Id);
-                        self.Name(data.Name);
-                        self.CountryId(data.CountryId);
-                        self.CountryName(data.CountryName);
-                        self.PositionId(data.PositionId);
-                        self.PositionName(data.PositionName);
-                        self.Photo(data.Photo);
-                    });
-                };
-
-                //--- Internal functions
-                function ajaxHelper(uri, method, data) {
-                    self.error(''); // Clear error message
-                    return $.ajax({
-                        type: method,
-                        url: uri,
-                        dataType: 'json',
-                        contentType: 'application/json',
-                        data: data ? JSON.stringify(data) : null,
-                        error: function (jqXHR, textStatus, errorThrown) {
-                            console.log("AJAX Call[" + uri + "] Fail...");
-                            hideLoading();
-                            self.error(errorThrown);
-                        }
-                    });
-                }
-
-                function showLoading() {
-                    $('#myModal').modal('show', {
-                        backdrop: 'static',
-                        keyboard: false
-                    });
-                }
-                function hideLoading() {
-                    $('#myModal').on('shown.bs.modal', function (e) {
-                        $("#myModal").modal('hide');
-                    })
-                }
-
-                function getUrlParameter(sParam) {
-                    var sPageURL = window.location.search.substring(1),
-                        sURLVariables = sPageURL.split('&'),
-                        sParameterName,
-                        i;
-
-                    for (i = 0; i < sURLVariables.length; i++) {
-                        sParameterName = sURLVariables[i].split('=');
-
-                        if (sParameterName[0] === sParam) {
-                            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-                        }
-                    }
-                };
-
-                //--- start ....
-                showLoading();
-                var pg = getUrlParameter('id');
-                console.log(pg);
-                if (pg == undefined)
-                    self.activate(1);
-                else {
-                    self.activate(pg);
-                }
-                console.log("VM initialized!");
-            };
-            $(".favourites").innerHTML +=
-                '<div data-bind="foreach: records" class="row" id="Players">' +
-                '<div class="card mb-3" style="max-width: 400px; margin-right: 5px; margin-bottom: 5px;">' +
-                '<div class="row g-0">' +
-                '<div class="col-md-7">' +
-                '<div class="card-body">' +
-                '<h5 class="card-title" data-bind="text: Name"></h5>' +
-                '<p class="card-text" data-bind="text: CountryName">' +
-                '<a href="./countryDetails.html?id=" class="nav-link"' +
-                'data-bind="text: CountryName, attr: { href:"./countryDetails.html?id=" + CountryId }"' +
-                'title="Show Country Details"></a>' +
-                '</p>' +
-                '<p class="card-text">' +
-                '<small class="text-body-secondary" data-bind="text: PositionName">' +
-                '<a href="./positionDetails.html?id=" class="nav-link"' +
-                'data-bind="text: PositionName, attr: { href:' + './positionDetails.html?id=' + ' + PositionId }"' +
-                'title="Show Position Details"></a>' +
-                '</small>' +
-                '</p>' +
-                '<div class="fixed">' +
-                '<a href="./PlayersDetails.html" class="btn btn-primary"' +
-                'data-bind="attr: { href:"+"./PlayersDetails.html?id=" + "Id" }">Show Details</a>' +
-                '</div>' +
-                '</div>' +
-                '</div>' +
-                '<div class="col-md-5" style="margin: none;">' +
-                '<!-- ko if: Photo === null -->' +
-                '<img src="images/logo players.png" alt data-bind="text: Name" class="card-img-top"' +
-                'style="width: 168px; height: 185px;border-radius:5px ">' +
-                '<!-- /ko -->' +
-                '<!-- ko ifnot: Photo === null -->' +
-                '<img data-bind="attr: { src: Photo, alt: "Name" }" class="card-img-top"' +
-                'style="width: 168px; height: 185px;border-radius:5px ">' +
-                '<!-- /ko -->' +
-                '</div>' +
-                '</div>' +
-
-                '</div>' +
-                '</div>'
-
-
-        };
-    };
-});
--- forAllPages.js
+++ forAllPages.js
@@ -5,57 +5,85 @@ const navbar = document.querySelector('.navbar');
 var darkmode_state = localStorage.getItem("darkmode_state", darkmode_state);
 console.log(darkmode_state)
 
-darkmodeToggle.addEventListener('change', function() {
+darkmodeToggle.addEventListener('change', function () {
   if (this.checked) {
     background.style.background = 'hsl(0, 1%, 14%)';
     background.style.color = 'rgb(192,192,192)';
-
+    $('nav').removeClass("navbar navbar-expand-lg navbar-light bg-light")
+    $('nav').addClass("navbar navbar-expand-lg navbar-dark bg-dark")
     $('.navbar').css('background-color', 'hsl(0, 1%, 14%)');
     $('.navbar').css('color', 'rgb(192,192,192)');
-    darkmode_state =1;
+    darkmode_state = 1;
     $(".fa-sun").css({ left: "5px", color: "#7e7e7e", transition: "0.3s" });
     $(".fa-moon").css({ left: "60px", color: "#fff", transition: "0.3s" });
+    $('.card').css('background-color', "gray");
+    $('.page-link').css("background-color", "rgb(40, 40, 40)");
+    $("tr:even").css("background-color", "rgb(40, 40, 40)");
+    $("tr:odd").css("background-color", "gray");
+    $('#drop').css("background-color", "rgb(40, 40, 40)")
 
 
   } else {
-    $(".fa-sun").css({ left: "5px", color: " #fff"  , transition: "0.3s" });
+    $(".fa-sun").css({ left: "5px", color: " #fff", transition: "0.3s" });
     $(".fa-moon").css({ left: "60px", color: "#7e7e7e", transition: "0.3s" });
     background.style.background = 'hsl(0, 0%, 100%)';
     background.style.color = 'black';
+    $('nav').removeClass("navbar navbar-expand-lg navbar-dark bg-dark")
+    $('nav').addClass("navbar navbar-expand-lg navbar-light bg-light")
     $('.navbar').css('background-color', 'hsl(0, 0%, 100%)');
+    $('.card').css('background-color', "white");
+    $('.page-link').css("background-color", "white");
+    $('.page-link:first').css("background-color", "blue");
+    $('#drop').css("background-color", "white")
+
+
+
+
+
     darkmode_state = 0
 
 
   }
   localStorage.setItem("darkmode_state", darkmode_state)
 });
-$(document).ready(function() {
+$(document).ready(function () {
 
-    if (darkmode_state==1){
-        background.style.background = 'hsl(0, 1%, 14%)';
-        background.style.color = 'rgb(192,192,192)';
-        darkmode_state =1
+  if (darkmode_state == 1) {
+    background.style.background = 'hsl(0, 1%, 14%)';
+    background.style.color = 'rgb(192,192,192)';
+    darkmode_state = 1
+    $('nav').removeClass("navbar navbar-expand-lg navbar-light bg-light")
+    $('nav').addClass("navbar navbar-expand-lg navbar-dark bg-dark")
+    $('.nav').removeClass("navbar-light bg-light")
+    $('.nav').addClass("navbar-dark bg-dark")
+    $('.nav').css('color', 'rgb(192,192,192)');
+    $('#darkmode-toggle').css('background', "linear-gradient(180deg, #777, #3a3a3a)");
+    $(".fa-sun").css({ left: "5px", color: "#7e7e7e", transition: "0.3s" });
+    $(".fa-moon").css({ left: "60px", color: "#fff", transition: "0.3s" });
+    $('#switch_mode').css("background-color", "#242424");
+    $('.card').css('background-color', "gray")
+    $('.page-link').css("background-color", "rgb(40, 40, 40)");
+    $('#drop').css("background-color", "rgb(40, 40, 40)")
 
-        $('.navbar').css('background-color', 'hsl(0, 1%, 14%)');
-        $('.navbar').css('color', 'rgb(192,192,192)');
-        $('#darkmode-toggle').css('background', "linear-gradient(180deg, #777, #3a3a3a)");
-        $(".fa-sun").css({ left: "5px", color: "#7e7e7e", transition: "0.3s" });
-        $(".fa-moon").css({ left: "60px", color: "#fff", transition: "0.3s" });
-        $('#darkmode-toggle').css("background", "#242424");
 
+  }
+  else {
+    $('nav').removeClass("navbar navbar-expand-lg navbar-dark bg-dark")
+    $('nav').addClass("navbar navbar-expand-lg navbar-light bg-light")
+    $('#drop').css("background-color", "white")
 
+  }
+  $('#arenasbutton').hover(function () {
+    $('#drop').slideDown(500);
+    //console.log($('#arenasbutton:hover').length)
+    //console.log($('#drop:hover').length)
+    if ($('#arenasbutton:hover').length === 0 && $('#drop:hover').length === 0) {
+      $('#drop').slideUp(500);
     }
-    $('#arenasbutton').hover(function() {
-        $('#drop').slideDown(500);
-        //console.log($('#arenasbutton:hover').length)
-        //console.log($('#drop:hover').length)
-        if ($('#arenasbutton:hover').length === 0 && $('#drop:hover').length===0 ){
-            $('#drop').slideUp(500);
-        }
-    });
-    $('#drop').hover(function() {
-        if ($('#arenasbutton:hover').length === 0 && $('#drop:hover').length===0 ){
-            $('#drop').slideUp(500);
-        }
-    });
-    });
+  });
+  $('#drop').hover(function () {
+    if ($('#arenasbutton:hover').length === 0 && $('#drop:hover').length === 0) {
+      $('#drop').slideUp(500);
+    }
+  });
+});
